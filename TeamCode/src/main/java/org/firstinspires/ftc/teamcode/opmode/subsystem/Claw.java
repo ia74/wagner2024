01a795fc7e8791b6ef0f23fa2499fd8731b43401
aiden <aidenjs9@icloud.com>
2025-01-08 08:40:55 -0600

    change position of writs down

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/subsystem/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/subsystem/Claw.java
@@ -19,7 +19,7 @@ public class Claw extends Subsystem {
 
     public static double wristUpPosition = 0.8;
     public static double wristMiddlePosition = 0.4;
-    public static double wristDownPosition = 0.1;
+    public static double wristDownPosition = 0.075;
 
     public Claw(HardwareMap hardwareMap) {
         super(hardwareMap);
