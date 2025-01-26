83d8da56c6b664128d49ba621dd7af5f27b8e4c4
ia74 <68617740+ia74@users.noreply.github.com>
2025-01-26 17:18:00 -0600

    weee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/colors/KSGay.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/colors/KSGay.java
@@ -6,9 +6,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.opmode.KneeSurgery;
 import org.firstinspires.ftc.teamcode.opmode.subsystem.Lights;
 
-@TeleOp(name = "Knee Surgery Lava")
-public class KSLava extends KneeSurgery {
+@TeleOp(name = "Knee Surgery Gay")
+public class KSGay extends KneeSurgery {
     public void setLightColor(Lights e) {
-        e.setPattern(RevBlinkinLedDriver.BlinkinPattern.BEATS_PER_MINUTE_LAVA_PALETTE);
+        e.setPattern(RevBlinkinLedDriver.BlinkinPattern.BEATS_PER_MINUTE_RAINBOW_PALETTE);
     }
 }
